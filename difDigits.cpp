a835dbbab8d4128a27ffecce7b32c1f754542c3b
duygunshot <60246562+duygunshot@users.noreply.github.com>
2022-11-06 13:48:24 -0600

    Update difDigits.cpp

--- difDigits.cpp
+++ difDigits.cpp
@@ -6,7 +6,7 @@ int main(){
    
    vector<int> v;
 
-   for (int a = 0; a <= 9; a++){
+   for (int a = 1; a <= 9; a++){
       for (int b = 0 ; b <= 9; b++){
          for (int c = 0;c <= 9; c++){
             for(int d = 0; d <= 9; d++){
