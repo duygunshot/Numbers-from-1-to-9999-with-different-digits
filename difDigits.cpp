701154278bef73e2df11d8bb3be848081587d9d6
duygunshot <60246562+duygunshot@users.noreply.github.com>
2022-11-06 13:32:05 -0600

    Add files via upload

---
+++ difDigits.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+   
+   vector<int> v;
+
+   for (int a = 0; a <= 9; a++){
+      for (int b = 0 ; b <= 9; b++){
+         for (int c = 0;c <= 9; c++){
+            for(int d = 0; d <= 9; d++){
+               if (d == c || d == b || d == a || c == b || c == a || b == a){
+                  continue;
+               }
+               else {
+                  v.push_back(a * 1000 + b * 100 + c * 10 + d );
+                  }
+               }
+            }
+         }
+      }
+   
+   vector<int>::iterator it;
+   for ( it = v.begin(); it != v.end(); it++){
+      cout << *it << endl;
+   }
+}
